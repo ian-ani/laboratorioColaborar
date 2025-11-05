6da2b8cc05f4ad4ac4a2b596e668216c14fa1994
markoperisic006-lang <markoperisic006@gmail.com>
2025-11-05 11:53:59 +0100

    Nueva linea

--- HolaMundo/src/Main.java
+++ HolaMundo/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde la práctica de Entornos!");
         System.out.println(" Modificacion");
-        System.out.println("Otra modificación del proyecto.");
+        System.out.println("Otra modificación del proyecto, zumo de manzana. Wooooo");
     }
 }
