b4712f58fcd235b0694e9d4c2bbdad6d923f46bb
markoperisic006-lang <markoperisic006@gmail.com>
2025-11-12 12:01:46 +0100

    cambio realizado

--- HolaMundo/src/utils/EntradaSalida.java
+++ HolaMundo/src/utils/EntradaSalida.java
@@ -31,6 +31,7 @@ public class EntradaSalida {
         while (!sc.hasNextInt()) {
             escribirMensajeError("El valor introducido no es un número entero.\nInténtalo de nuevo: Hola ");
             sc.next();
+            System.out.println("Esqueletos y dragones del mine dificil de pasar, nether sin los ojos de enderman no se puede encontrar el portal,recuerdas salir del juego y se borra el mundo has perdido todo el progreso,tanto esfuerzo para nada eso mismo pasa en la vida cotidiana, las cosas que hacemos no siempre salen bien pero al final uno hacepta lo que pasa con la sociedad, no todas las personas somos iguales a algunas a un tienen algo a lo que aferrarse otros deciden que este mundo no es para ellos al final a todos nos pasa lo mismo pero hay diferentes maneras de crear una historia, cada uno crea la suya propia lo unico que nos hace diferentes son los pensamientos y como actuamos frente a algo que choca en nuestros pensaminetos.");
         }
 
         return sc.nextInt();
